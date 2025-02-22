c14565ba97684707c88ed2221646dcc92bf55b19
manvi0308 <chaddhamanvi3@gmail.com>
2025-02-22 14:15:46 +0530

    first commit

---
+++ src/Components/TicTaeToe.java
@@ -0,0 +1,5 @@
+package Components;
+
+public class TicTaeToe extends Board{
+    String[][] cells = new String[3][3];
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import Components.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public Board start(){
+        return new Board();
+    }
+
+    public void move(Board board, Player player, Move move){
+
+    }
+
+    public GameResult isComplete(Board board){
+        // whether game is complete or not complete
+        if(board instanceof TicTaeToe){
+
+        }
+    }
+}
